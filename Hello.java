b650a3bf9042d7af0df80751bf59a5b4872871c4
mahajan854 <mahajannilesh854@gmail.com>
2023-04-26 11:25:06 +0530

    done

--- Hello.java
+++ Hello.java
@@ -3,8 +3,8 @@
 	 
 		public static void main(String[] args)
 		{
-		   int a=10;
-		   int b=20;
+		   int a=20;
+		   int b=10;
 		   int c=a+b;
                int d=a/b;
 		   System.out.println("Addition:"+c);

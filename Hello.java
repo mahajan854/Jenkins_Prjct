4b19537875c409e4046f0fc7b1e287c46cbf1586
mahajan854 <mahajannilesh854@gmail.com>
2023-04-26 11:23:06 +0530

    done

--- Hello.java
+++ Hello.java
@@ -6,7 +6,7 @@
 		   int a=10;
 		   int b=20;
 		   int c=a+b;
-               int d=a/b
+               int d=a/b;
 		   System.out.println("Addition:"+c);
                System.out.println("Division:"+d);
 	         
